146f54346d40beb40821baeff9069a9809b59f28
Heltion <155143181+Helt-Ion@users.noreply.github.com>
2024-03-14 22:36:49 +0800

    Update settings.py

--- project/font_detection_web/server/server/settings.py
+++ project/font_detection_web/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sp$j706lt-kv51$o2lhgh6qnxfktqsge*@@x*d048jg1u!dosp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
